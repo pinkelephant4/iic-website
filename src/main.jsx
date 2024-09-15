c4f33d09db634d577ff0f75218859536d75f5e29
hardiksingh <officialhardik761@gmail.com>
2024-09-15 15:20:07 +0530

    14th

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import MouseAnimation from "./components/MouseAnimation.jsx";
 // import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <MouseAnimation/>
     <App />
   </StrictMode>,
 )
