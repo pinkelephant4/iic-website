ec511b3468a59788526a96e7ea83fdfbf429a94d
hardiksingh <officialhardik761@gmail.com>
2024-09-22 12:59:05 +0530

    Sponsor updated

--- src/components/Sponser.jsx
+++ src/components/Sponser.jsx
@@ -1,34 +1,36 @@
 import "../styles/Sponser.css";
-import Person from "./Person";
 
 const Sponser = () => {
   return (
     <div className="sponser">
       <div className="sponser-header">
-        <h1>IIC OFFICIAL SPONSERS</h1>
+        <h1>IIC Official Sponsors</h1>
       </div>
       <div className="sponser-body">
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        <h1 className="heading">Knowledge Partner</h1>
-        <section>
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        </section>
-        <h1 className="heading">Startup Partner</h1>
-        <section>
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        </section>
-        <h1 className="heading">Technical Partner</h1>
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        <h1 className="heading">Healthcare Partner</h1>
-        <section>
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        <Person name={'Hardik'} about={'A Great person and a Scientist'} post={'President of India'} img={'https://images.unsplash.com/photo-1617718860170-dd5d9f2ed43d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBqJTIwYWJkdWwlMjBrYWxhbXxlbnwwfDF8MHx8fDA%3D'}/>
-        </section>
+        <div className="knowledge-card">
+          <h1>Knowledge Partner</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+            voluptates soluta velit.
+          </p>
+        </div>
+        <div className="education-card1">
+          <h1>Education Partner 1</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+            voluptates soluta velit.
+          </p>
+        </div>
+        <div className="education-card2">
+          <h1>Education Partner 2</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+            voluptates soluta velit.
+          </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sponser
+export default Sponser;
