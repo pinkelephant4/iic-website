d4e7360efdb351fe0cb02babb898f29707a094c9
Ananya Maheshwari <ananyamaheshwari2004@gmail.com>
2024-09-22 14:38:26 +0530

    changed nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/Navbar.css'; // Ensure this import is at the top of your file
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import '../styles/Navbar.css';
+
 
 const Navbar = () => {
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-  const [hidden, setHidden] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-      // Scrolling down
-      setHidden(true);
-    } else {
-      // Scrolling up
-      setHidden(false);
-    }
-    setLastScrollTop(scrollTop <= 0 ? 0 : scrollTop); // For Mobile or negative scrolling
-  };
+  const [scrolling, setScrolling] = useState(false);
 
   useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 40) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
     window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [lastScrollTop]);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className={`navbar ${hidden ? 'navbar-hidden' : ''}`}>
+    <div className={`navbar ${scrolling ? 'scrolled' : ''}`}>
       <div className="nav-left">
-        <h1>IIC</h1>
+        <img src="/assets/iic logo bg removed 2.png" alt="" />
       </div>
-      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/timeline">Timeline</Link>
-        <Link to="/ambassador">Ambassador</Link>
-        <Link to="/team">Team</Link>
-        <Link to="/gallery">Gallery</Link>
-        <Link to="/faqs">Faqs</Link>
+      <div className="nav-mid">
+        <div className="nav-links">
+          <div className="nav-link active">Home</div>
+          <div className="nav-link">About</div>
+          <div className="nav-link">Team</div>
+        </div>
       </div>
       <div className="nav-right">
-        <a href="">Problem Statements</a>
-      </div>
-      <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-        ☰
+        <div className="nav-right-btn">
+          <span>
+            Problem Statments
+          </span>
+          <img src="/assets/right-arrow.svg" alt="" />
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
