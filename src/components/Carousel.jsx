072b2da032e3a862cda8201aa375117d9d716c4e
princelakhwani <princelakhwani1619@gmail.com>
2024-09-23 15:18:31 +0530

    Prince 98

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,6 +7,7 @@ import logo3 from "/Theme/BlockChain.png";
 import logo4 from "/Theme/IOT.png";
 import logo5 from "/Theme/OpenInnovation.png";
 import logo6 from "/Theme/World.png";
+import MagneticEffect from "./Magnetic";
 
 const Carousel = () => {
   return (
@@ -14,39 +15,51 @@ const Carousel = () => {
       <h1>Themes</h1>
       
       <div className="themes-row">
+      <MagneticEffect>  
         <div className="theme-card">
           <img src={logo1} alt="Theme 1 Logo" className="theme-logo" />
-          <p className="theme-name">Theme 1</p>
+          <p className="theme-name">Health Care</p>
           <p className="theme-description">Description of Theme 1. This is a three-line description to explain the theme.</p>
         </div>
+      </MagneticEffect>
+      <MagneticEffect>
         <div className="theme-card">
           <img src={logo2} alt="Theme 2 Logo" className="theme-logo" />
-          <p className="theme-name">Theme 2</p>
+          <p className="theme-name">Space Tech</p>
           <p className="theme-description">Description of Theme 2. This is a three-line description to explain the theme.</p>
         </div>
+      </MagneticEffect>
+      <MagneticEffect>
         <div className="theme-card">
           <img src={logo3} alt="Theme 3 Logo" className="theme-logo" />
-          <p className="theme-name">Theme 3</p>
+          <p className="theme-name">Block Chain</p>
           <p className="theme-description">Description of Theme 3. This is a three-line description to explain the theme.</p>
         </div>
+      </MagneticEffect>   
       </div>
       
       <div className="themes-row">
+      <MagneticEffect>  
         <div className="theme-card">
           <img src={logo4} alt="Theme 4 Logo" className="theme-logo" />
-          <p className="theme-name">Theme 4</p>
+          <p className="theme-name">IOT Tech</p>
           <p className="theme-description">Description of Theme 4. This is a three-line description to explain the theme.</p>
         </div>
+      </MagneticEffect>  
+      <MagneticEffect>
         <div className="theme-card">
           <img src={logo5} alt="Theme 5 Logo" className="theme-logo" />
-          <p className="theme-name">Theme 5</p>
+          <p className="theme-name">Open Innovation</p>
           <p className="theme-description">Description of Theme 5. This is a three-line description to explain the theme.</p>
         </div>
+      </MagneticEffect>  
+      <MagneticEffect>
         <div className="theme-card">
           <img src={logo6} alt="Theme 6 Logo" className="theme-logo" />
-          <p className="theme-name">Theme 6</p>
+          <p className="theme-name">Environmental</p>
           <p className="theme-description">Description of Theme 6. This is a three-line description to explain the theme.</p>
         </div>
+      </MagneticEffect>  
       </div>
     </div>
   );
