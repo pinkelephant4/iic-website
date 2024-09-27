7fcd939c8cec4e8abc16ff4bb5720ad7dabfd2cd
swarnajain0 <jainswar28@gmail.com>
2024-09-28 02:01:17 +0530

    swarnaa

--- src/components/StudentConvener.jsx
+++ src/components/StudentConvener.jsx
@@ -3,10 +3,10 @@ import '../styles/StudentConvener.css';
 const StudentConvener = () => {
   return (
     <div className='StudentConvener'>
-        <div className="header-sc">
+        <div className="heading">
             <h1>Student Convener 2024-25</h1>
         </div>  
-        <div className="content-sc">
+        <div className="container-sc">
             <div className="box-sc">Sample</div>
             <div className="box-sc"></div>
             <div className="box-sc"></div>
