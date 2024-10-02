5783d3f666923d1181061f2f54cc5c5c1d0f9eed
swarnajain0 <jainswar28@gmail.com>
2024-10-02 16:06:41 +0530

    swarnaa

--- src/components/CampusLast.jsx
+++ src/components/CampusLast.jsx
@@ -9,29 +9,30 @@ const CampusLast = () => {
       <div className="CampusLast-box">
         <img src="path_to_your_icon/icon1.png" alt="icon" className="icon" />
         <div className="CampusLast-box-number">1</div>
-        <h2>Heading 1</h2>
-        <p>This is some text inside box 1.</p>
+        <h2>AIC Incubation</h2>
+        <p>Top 1-2 ambassadors will get the change to incubate at Atal Incubation Center</p>
       </div>
       <div className="CampusLast-box">
         <img src="path_to_your_icon/icon2.png" alt="icon" className="icon" />
         <div className="CampusLast-box-number">2</div>
-        <h2>Heading 2</h2>
-        <p>This is some text inside box 2.</p>
+        <h2>Exclusive Goodies</h2>
+        <p>Top 5 campus ambassador will receive exclusive goodies.</p>
       </div>
       <div className="CampusLast-box">
         <img src="path_to_your_icon/icon3.png" alt="icon" className="icon" />
         <div className="CampusLast-box-number">3</div>
-        <h2>Heading 3</h2>
-        <p>This is some text inside box 3.</p>
+        <h2>Letter of Recommendation </h2>
+        <p>Campus ambassadors who successfully bring 20+ teams will be eligible for LOR</p>
       </div>
       <div className="CampusLast-box">
         <img src="path_to_your_icon/icon4.png" alt="icon" className="icon" />
         <div className="CampusLast-box-number">4</div>
-        <h2>Heading 4</h2>
-        <p>This is some text inside box 4.</p>
+        <h2>Certificate</h2>
+        <p>CAs who bring in 5+ registrations will be awarded with a certificate of appreciation</p>
     </div>
 
     </div>
+    <h1>You are just 3 Steps away from <br></br> becoming a part of this journey </h1>
 
     </div>
   );
