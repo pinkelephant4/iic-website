8ab2566abd8304a85a644cdbd2425e228d102ad7
hardiksingh <officialhardik761@gmail.com>
2024-10-12 11:02:41 +0530

    about page done

--- src/components/AboutCards.jsx
+++ src/components/AboutCards.jsx
@@ -1,42 +1,175 @@
 import { useState } from "react";
 import "../styles/AboutCards.css";
+import jQuery from "jquery";
 
 const AboutCards = () => {
-  const [activeIndex, setActiveIndex] = useState(2); // Initial middle card is the third one
+  (function ($) {
+    $.fn.cascadeSlider = function (opt) {
+      var $this = this,
+        itemClass = opt.itemClass || "cascade-slider_item",
+        arrowClass = opt.arrowClass || "cascade-slider_arrow",
+        $item = $this.find("." + itemClass),
+        $arrow = $this.find("." + arrowClass),
+        itemCount = $item.length;
 
-  const cards = [
-    { id: 1, content: "Experience 1" },
-    { id: 2, content: "Experience 2" },
-    { id: 3, content: "Experience 3" }, // Middle card
-    { id: 4, content: "Experience 4" },
-    { id: 5, content: "Experience 5" },
-  ];
+      var defaultIndex = 0;
 
-  const handleCardClick = (index) => {
-    setActiveIndex(index);
-  };
+      changeIndex(defaultIndex);
+
+      $arrow.on("click", function () {
+        var action = $(this).data("action"),
+          nowIndex = $item.index($this.find(".now"));
+
+        if (action == "next") {
+          if (nowIndex == itemCount - 1) {
+            changeIndex(0);
+          } else {
+            changeIndex(nowIndex + 1);
+          }
+        } else if (action == "prev") {
+          if (nowIndex == 0) {
+            changeIndex(itemCount - 1);
+          } else {
+            changeIndex(nowIndex - 1);
+          }
+        }
+
+        $(".cascade-slider_dot").removeClass("cur");
+        //$('.cascade-slider_dot').next().addClass('cur');
+      });
+
+      // add data attributes
+      for (var i = 0; i < itemCount; i++) {
+        $(".cascade-slider_item").each(function (i) {
+          $(this).attr("data-slide-number", [i]);
+        });
+      }
+
+      // dots
+      $(".cascade-slider_dot").bind("click", function () {
+        // add class to current dot on click
+        $(".cascade-slider_dot").removeClass("cur");
+        $(this).addClass("cur");
+
+        var index = $(this).index();
+
+        $(".cascade-slider_item").removeClass("now prev next");
+        var slide = $(".cascade-slider_slides").find(
+          "[data-slide-number=" + index + "]"
+        );
+        slide.prev().addClass("prev");
+        slide.addClass("now");
+        slide.next().addClass("next");
+
+        if (slide.next().length == 0) {
+          $(".cascade-slider_item:first-child").addClass("next");
+        }
+
+        if (slide.prev().length == 0) {
+          $(".cascade-slider_item:last-child").addClass("prev");
+        }
+      });
+
+      function changeIndex(nowIndex) {
+        // clern all class
+        $this.find(".now").removeClass("now");
+        $this.find(".next").removeClass("next");
+        $this.find(".prev").removeClass("prev");
+        if (nowIndex == itemCount - 1) {
+          $item.eq(0).addClass("next");
+        }
+        if (nowIndex == 0) {
+          $item.eq(itemCount - 1).addClass("prev");
+        }
+
+        $item.each(function (index) {
+          if (index == nowIndex) {
+            $item.eq(index).addClass("now");
+          }
+          if (index == nowIndex + 1) {
+            $item.eq(index).addClass("next");
+          }
+          if (index == nowIndex - 1) {
+            $item.eq(index).addClass("prev");
+          }
+        });
+      }
+    };
+  })(jQuery);
+
+  $("#cascade-slider").cascadeSlider({
+    itemClass: "cascade-slider_item",
+    arrowClass: "cascade-slider_arrow",
+  });
 
   return (
-    <div className="about-cards">
-      <h1>
-        Our Previous <br /> Experiences
-      </h1>
-      <div className="carousel">
-        {cards.map((card, index) => (
-          <div
-            key={card.id}
-            className={`carousel-card ${
-              index === activeIndex ? "active" : ""
-            } ${index < activeIndex ? "left" : "right"} ${
-              index === 0 || index === 4 ? "end" : " "
-            }`}
-            onClick={() => handleCardClick(index)}
-          >
-            <p>{card.content}</p>
+    <>
+      <div className="about-cards">
+        <h1>
+          Our Previous <br /> Experiences
+        </h1>
+        <div className="cascade-slider_container" id="cascade-slider">
+          <div className="cascade-slider_slides">
+            <div className="cascade-slider_item">
+              <div className="card align">
+                <div className="photo">
+                  <img src="/assets/about-1.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="cascade-slider_item">
+              <div className="card align">
+                <div className="photo">
+                  <img src="/assets/about-1.jpg" />
+                </div>
+              </div>
+            </div>
+            <div className="cascade-slider_item">
+              <div className="card align">
+                <div className="photo">
+                  <img src="/assets/about-1.jpg" />
+                </div>
+              </div>
+            </div>
+            <div className="cascade-slider_item">
+              <div className="card align">
+                <div className="photo">
+                  <img src="/assets/about-1.jpg" />
+                </div>
+              </div>
+            </div>
+            <div className="cascade-slider_item">
+              <div className="card align">
+                <div className="photo">
+                  <img src="/assets/about-1.jpg" />
+                </div>
+              </div>
+            </div>
           </div>
-        ))}
+
+          <ol className="cascade-slider_nav">
+            <li className="cascade-slider_dot cur"></li>
+            <li className="cascade-slider_dot"></li>
+            <li className="cascade-slider_dot"></li>
+            <li className="cascade-slider_dot"></li>
+            <li className="cascade-slider_dot"></li>
+          </ol>
+
+          <span
+            className="cascade-slider_arrow cascade-slider_arrow-left"
+            data-action="prev"
+          >
+            <img src="/assets/right-arrow.svg" type="image/svg+xml" />
+          </span>
+          <span
+            className="cascade-slider_arrow cascade-slider_arrow-right"
+            data-action="next"
+          >
+            <img src="/assets/right-arrow.svg" type="image/svg+xml" />
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
