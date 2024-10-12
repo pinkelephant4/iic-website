b40fda02800f7c16f7d55834628453148234c013
princelakhwani <princelakhwani1619@gmail.com>
2024-10-12 13:05:40 +0530

    Prince

--- src/components/AboutCards.jsx
+++ src/components/AboutCards.jsx
@@ -1,175 +1,121 @@
-import { useState } from "react";
-import "../styles/AboutCards.css";
+import { useState, useEffect } from "react";
 import jQuery from "jquery";
+import "../styles/AboutCards.css";
 
 const AboutCards = () => {
-  (function ($) {
-    $.fn.cascadeSlider = function (opt) {
-      var $this = this,
-        itemClass = opt.itemClass || "cascade-slider_item",
-        arrowClass = opt.arrowClass || "cascade-slider_arrow",
-        $item = $this.find("." + itemClass),
-        $arrow = $this.find("." + arrowClass),
-        itemCount = $item.length;
+  const [currentIndex, setCurrentIndex] = useState(0); // Track current card index
+  const totalItems = 5; // Total number of cards
 
-      var defaultIndex = 0;
+  // Array of image sources for each card
+  const cardImages = [
+    "/assets/about-6.png",
+    "/assets/about-2.png",
+    "/assets/about-3.png",
+    "/assets/about-4.png",
+    "/assets/about-5.png",
+  ];
 
-      changeIndex(defaultIndex);
+  useEffect(() => {
+    // Initialize jQuery slider logic after component mounts
+    (function ($) {
+      $.fn.cascadeSlider = function () {
+        const $slider = $("#cascade-slider");
+        const $arrows = $slider.find(".cascade-slider_arrow");
 
-      $arrow.on("click", function () {
-        var action = $(this).data("action"),
-          nowIndex = $item.index($this.find(".now"));
-
-        if (action == "next") {
-          if (nowIndex == itemCount - 1) {
-            changeIndex(0);
-          } else {
-            changeIndex(nowIndex + 1);
-          }
-        } else if (action == "prev") {
-          if (nowIndex == 0) {
-            changeIndex(itemCount - 1);
-          } else {
-            changeIndex(nowIndex - 1);
-          }
-        }
-
-        $(".cascade-slider_dot").removeClass("cur");
-        //$('.cascade-slider_dot').next().addClass('cur');
-      });
-
-      // add data attributes
-      for (var i = 0; i < itemCount; i++) {
-        $(".cascade-slider_item").each(function (i) {
-          $(this).attr("data-slide-number", [i]);
+        // Attach event handlers
+        $arrows.on("click", function () {
+          const action = $(this).data("action");
+          handleArrowClick(action);
         });
-      }
 
-      // dots
-      $(".cascade-slider_dot").bind("click", function () {
-        // add class to current dot on click
-        $(".cascade-slider_dot").removeClass("cur");
-        $(this).addClass("cur");
-
-        var index = $(this).index();
-
-        $(".cascade-slider_item").removeClass("now prev next");
-        var slide = $(".cascade-slider_slides").find(
-          "[data-slide-number=" + index + "]"
-        );
-        slide.prev().addClass("prev");
-        slide.addClass("now");
-        slide.next().addClass("next");
-
-        if (slide.next().length == 0) {
-          $(".cascade-slider_item:first-child").addClass("next");
-        }
-
-        if (slide.prev().length == 0) {
-          $(".cascade-slider_item:last-child").addClass("prev");
-        }
-      });
-
-      function changeIndex(nowIndex) {
-        // clern all class
-        $this.find(".now").removeClass("now");
-        $this.find(".next").removeClass("next");
-        $this.find(".prev").removeClass("prev");
-        if (nowIndex == itemCount - 1) {
-          $item.eq(0).addClass("next");
-        }
-        if (nowIndex == 0) {
-          $item.eq(itemCount - 1).addClass("prev");
-        }
-
-        $item.each(function (index) {
-          if (index == nowIndex) {
-            $item.eq(index).addClass("now");
-          }
-          if (index == nowIndex + 1) {
-            $item.eq(index).addClass("next");
-          }
-          if (index == nowIndex - 1) {
-            $item.eq(index).addClass("prev");
-          }
+        $(".cascade-slider_dot").on("click", function () {
+          const index = $(this).index();
+          setCurrentIndex(index); // Update state with clicked dot index
         });
-      }
-    };
-  })(jQuery);
+      };
+    })(jQuery);
 
-  $("#cascade-slider").cascadeSlider({
-    itemClass: "cascade-slider_item",
-    arrowClass: "cascade-slider_arrow",
-  });
+    $("#cascade-slider").cascadeSlider();
+  }, []);
+
+  const handleArrowClick = (action) => {
+    setCurrentIndex((prevIndex) =>
+      action === "next"
+        ? (prevIndex + 1) % totalItems
+        : (prevIndex - 1 + totalItems) % totalItems
+    );
+  };
+
+  const getClassForItem = (index) => {
+    const position = (index - currentIndex + totalItems) % totalItems;
+
+    switch (position) {
+      case 0:
+        return "now";
+      case 1:
+        return "next";
+      case 2:
+        return "next-next";
+      case totalItems - 1:
+        return "prev";
+      case totalItems - 2:
+        return "prev-prev";
+      default:
+        return "";
+    }
+  };
 
   return (
-    <>
-      <div className="about-cards">
-        <h1>
-          Our Previous <br /> Experiences
-        </h1>
-        <div className="cascade-slider_container" id="cascade-slider">
-          <div className="cascade-slider_slides">
-            <div className="cascade-slider_item">
+    <div className="about-cards">
+      <h1>
+        Our Previous <br /> Experiences
+      </h1>
+      <div className="cascade-slider_container" id="cascade-slider">
+        <div className="cascade-slider_slides">
+          {Array.from({ length: totalItems }).map((_, index) => (
+            <div
+              className={`cascade-slider_item ${getClassForItem(index)}`}
+              key={index}
+            >
               <div className="card align">
                 <div className="photo">
-                  <img src="/assets/about-1.jpg" alt="" />
+                  {/* Render the corresponding image for each card */}
+                  <img
+                    src={cardImages[index]}
+                    alt={`About ${index + 1}`}
+                  />
                 </div>
               </div>
             </div>
-            <div className="cascade-slider_item">
-              <div className="card align">
-                <div className="photo">
-                  <img src="/assets/about-1.jpg" />
-                </div>
-              </div>
-            </div>
-            <div className="cascade-slider_item">
-              <div className="card align">
-                <div className="photo">
-                  <img src="/assets/about-1.jpg" />
-                </div>
-              </div>
-            </div>
-            <div className="cascade-slider_item">
-              <div className="card align">
-                <div className="photo">
-                  <img src="/assets/about-1.jpg" />
-                </div>
-              </div>
-            </div>
-            <div className="cascade-slider_item">
-              <div className="card align">
-                <div className="photo">
-                  <img src="/assets/about-1.jpg" />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <ol className="cascade-slider_nav">
-            <li className="cascade-slider_dot cur"></li>
-            <li className="cascade-slider_dot"></li>
-            <li className="cascade-slider_dot"></li>
-            <li className="cascade-slider_dot"></li>
-            <li className="cascade-slider_dot"></li>
-          </ol>
-
-          <span
-            className="cascade-slider_arrow cascade-slider_arrow-left"
-            data-action="prev"
-          >
-            <img src="/assets/right-arrow.svg" type="image/svg+xml" />
-          </span>
-          <span
-            className="cascade-slider_arrow cascade-slider_arrow-right"
-            data-action="next"
-          >
-            <img src="/assets/right-arrow.svg" type="image/svg+xml" />
-          </span>
+          ))}
         </div>
+
+        <ol className="cascade-slider_nav">
+          {Array.from({ length: totalItems }).map((_, index) => (
+            <li
+              className={`cascade-slider_dot ${
+                index === currentIndex ? "cur" : ""
+              }`}
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+            ></li>
+          ))}
+        </ol>
+
+        <span
+          className="cascade-slider_arrow cascade-slider_arrow-left"
+          data-action="prev"
+        >
+          <img src="/assets/left-arrow-svgrepo-com.svg" alt="Previous" />
+        </span>
+        <span
+          className="cascade-slider_arrow cascade-slider_arrow-right"
+          data-action="next"
+        >
+          <img src="/assets/right-arrow-svgrepo-com.svg" alt="Next" />
+        </span>
       </div>
-    </>
+    </div>
   );
 };
 
