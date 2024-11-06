f5982b1aadd0cfcbf3095ad183a129512c012c84
hardiksingh <officialhardik761@gmail.com>
2024-11-06 22:45:56 +0530

    added teams button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,7 @@
-import '../styles/Hero.css'
-import MagneticEffect from './Magnetic'
+import "../styles/Hero.css";
+import MagneticEffect from "./Magnetic";
 
 const Hero = () => {
-
   const days = () => {
     const today = new Date();
     const hackDate = new Date(today.getFullYear(), 10, 15);
@@ -10,17 +9,16 @@ const Hero = () => {
     const daysLeft = Math.ceil(difference / (1000 * 60 * 60 * 24));
 
     return daysLeft;
-  }
-
+  };
 
   return (
     <>
-      <div className='hero-wrapper'>
-        <img src="assets/Frame 5232.png" alt="" className='hero-bg-img' />
+      <div className="hero-wrapper">
+        <img src="assets/Frame 5232.png" alt="" className="hero-bg-img" />
         <div className="hero-heading">
-          <span className='hero-heading-l1'>International Innovation </span>
+          <span className="hero-heading-l1">International Innovation </span>
           <br />
-          <span className='hero-heading-l2'>Challenge</span>
+          <span className="hero-heading-l2">Challenge</span>
           <br />
 
           <div className="Hackathon">
@@ -30,12 +28,12 @@ const Hero = () => {
             </div>
             <div className="Hourglass-image">
               <img src="/assets/hourglass.png" alt="hourglass" />
-            </div>  
+            </div>
           </div>
 
-          <div className='unstop-div'>
+          <div className="unstop-div">
             <span>co-powered by</span>
-            <div className='unstop-img-div'>
+            <div className="unstop-img-div">
               <img src="/assets/unstop2.png" alt="" />
               {/* <div className='unstop'></div> */}
             </div>
@@ -43,31 +41,37 @@ const Hero = () => {
         </div>
         <div className="hero-bottom">
           <div className="hero-bottom-icons">
-
             <MagneticEffect>
               <a href="https://www.instagram.com/iicmuj?igsh=MnZsaWhsa2dqZXFl">
-              <img src="/assets/insta-b.svg" alt="Instagram" className='hero-bottom-icon' />
+                <img
+                  src="/assets/insta-b.svg"
+                  alt="Instagram"
+                  className="hero-bottom-icon"
+                />
               </a>
             </MagneticEffect>
 
             <MagneticEffect>
               <a href="">
-              <img src="/assets/linkedin-b.svg" alt="LinkedIn" className='hero-bottom-icon' />
+                <img
+                  src="/assets/linkedin-b.svg"
+                  alt="LinkedIn"
+                  className="hero-bottom-icon"
+                />
               </a>
             </MagneticEffect>
           </div>
 
-          <div className='hero-bottom-left'>
-            <span className='hero-bottom-left-text'>
-              Days
-            </span>
+          <div className="hero-bottom-left">
+            <span className="hero-bottom-left-text">Days</span>
             <br />
-            <span className='hero-bottom-left-number'>
-              {days()}
-            </span>
-            <span className='hero-bottom-left-text'>
-              Left
-            </span>
+            <span className="hero-bottom-left-number">{days()}</span>
+            <span className="hero-bottom-left-text">Left</span>
+            <div className="teams-button">
+              <a href="assets/teams.xlsx" download="Shortlisted_Teams.xlsx">
+                Shortlisted Teams
+              </a>
+            </div>
           </div>
           <div className="hero-bottom-img-container">
             <img
@@ -81,27 +85,36 @@ const Hero = () => {
               className="hero-bottom-img"
             />
           </div>
-          <div className='hero-bottom-right'>
+          <div className="hero-bottom-right">
             <div className="hero-bottom-right-container">
               <MagneticEffect>
                 <div className="hero-bottom-right-register-btn">
-                  <a href='https://unstop.com/hackathons/international-innovation-challengehackathon-manipal-university-jaipur-1174688?lb=CtnnOKei' className='hero-right-btn-link'> Register Now </a>
+                  <a
+                    href="https://unstop.com/hackathons/international-innovation-challengehackathon-manipal-university-jaipur-1174688?lb=CtnnOKei"
+                    className="hero-right-btn-link"
+                  >
+                    {" "}
+                    Register Now{" "}
+                  </a>
                 </div>
               </MagneticEffect>
-              <div className='hero-bottom-right-about' >
-
+              <div className="hero-bottom-right-about">
                 <span>About</span>
                 <p>
-                  The International Innovation Challenge (IIC) is a premier global hackathon that brings together bright minds from top universities to solve real-world industry challenges. Organized by Manipal University in collaboration with other key partners, IIC focuses on critical areas like cybersecurity, sustainability, and digital transformation.
+                  The International Innovation Challenge (IIC) is a premier
+                  global hackathon that brings together bright minds from top
+                  universities to solve real-world industry challenges.
+                  Organized by Manipal University in collaboration with other
+                  key partners, IIC focuses on critical areas like
+                  cybersecurity, sustainability, and digital transformation.
                 </p>
               </div>
-
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
