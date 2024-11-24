7d7851a677a6955ea46fa209d70b46df37bf0d84
hardiksingh <officialhardik761@gmail.com>
2024-11-24 21:31:39 +0530

    done

--- src/pages/Top10.jsx
+++ src/pages/Top10.jsx
@@ -12,46 +12,46 @@ const Top10 = ({ setActive }) => {
         </div>
         <div className="teamlist">
           <div className="teamname">
-            <h1>SILI</h1>
+            <h1>1st. SILI</h1>
             <h2>Norwegian University of Science and Technology, Norway</h2>
           </div>
           <div className="teamname">
-            <h1>Permutes</h1>
+            <h1>2nd. Permutes</h1>
             <h2>Manipal University (MUJ), Jaipur</h2>
           </div>
           <div className="teamname">
-            <h1>AeroVisionaries</h1>
+            <h1>3rd. AeroVisionaries</h1>
             <h2>Chandigarh University (CU), Punjab</h2>
           </div>
           <div className="teamname">
-            <h1>Tech Tribe</h1>
+            <h1>4. Tech Tribe</h1>
             <h2>G.H. Raisoni College of Engineering and Management, Pune</h2>
           </div>
           <div className="teamname">
-            <h1>Nexium</h1>
+            <h1>5. Nexium</h1>
             <h2>GD Goenka University, Gurugram</h2>
           </div>
           <div className="teamname">
-            <h1>Smirnoff</h1>
+            <h1>6. Smirnoff</h1>
             <h2>IMS Engineering College (IMSEC), Ghaziabad</h2>
           </div>
           <div className="teamname">
-            <h1>RYM Grenergy</h1>
+            <h1>7. RYM Grenergy</h1>
             <h2>GD Goenka University, Gurugram</h2>
           </div>
           <div className="teamname">
-            <h1>FARIYAAD</h1>
+            <h1>8. FARIYAAD</h1>
             <h2>Thapar Institute of Engineering & Technology, Patiala</h2>
           </div>
           <div className="teamname">
-            <h1>EthosCare</h1>
+            <h1>9. EthosCare</h1>
             <h2>
               Dr. Akhilesh Das Gupta Institute of Technology and Management, New
               Delhi
             </h2>
           </div>
           <div className="teamname">
-            <h1>Code invaders</h1>
+            <h1>10. Code invaders</h1>
             <h2>Manipal University (MUJ), Jaipur</h2>
           </div>
         </div>
